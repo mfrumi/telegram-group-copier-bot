efb6803179298cce30340ad73c12369e8e6003b5
farhan rumi <gunlimted736@gmail.com>
2025-09-06 16:33:06 +0500

    Update sanitizer.py

--- sanitizer.py
+++ sanitizer.py
@@ -1,1 +1,17 @@
+import re
 
+
+URL_RE = re.compile(r'https?://(t\.?me|telegram\.me|telegram\.dog)/[^\s]+', flags=re.IGNORECASE)
+AT_USERNAME_RE = re.compile(r'@\w{5,64}')
+INVITE_RE = re.compile(r'(https?://)?t\.me/joinchat/[^\s]+', flags=re.IGNORECASE)
+
+
+def remove_telegram_links(text: str) -> str:
+if not text:
+return text
+t = URL_RE.sub('', text)
+t = INVITE_RE.sub('', t)
+t = AT_USERNAME_RE.sub('', t)
+t = re.sub(r'\n\s+','\n', t)
+t = re.sub(r'\s{2,}',' ', t)
+return t.strip()
